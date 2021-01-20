ce2d5943308d6cfcb7e764adc602cdf615a99519
zukizaro <minhduong.zaro@gmail.com>
2021-01-20 15:56:18 +0700

    deploy-site

---
+++ precache-manifest.8f1096f75c9698ffa5c9f3af447d413e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b1cb7365f26c4d3717dfe147d8f6c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a85f7b7ada17f648d01",
+    "url": "/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "ad0f2d4f51c9410c4140",
+    "url": "/static/css/main.c9630788.chunk.css"
+  },
+  {
+    "revision": "1aff99a5cff69c23a9b8",
+    "url": "/static/js/2.9796fc41.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.9796fc41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a85f7b7ada17f648d01",
+    "url": "/static/js/3.324b976a.chunk.js"
+  },
+  {
+    "revision": "ad0f2d4f51c9410c4140",
+    "url": "/static/js/main.9ce3625a.chunk.js"
+  },
+  {
+    "revision": "ae5d15a22dee2f8bd38b",
+    "url": "/static/js/runtime-main.cdaf186d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "d2ec94e83d83ac505b3fcd1b0974d92b",
+    "url": "/static/media/developerActivity.d2ec94e8.svg"
+  },
+  {
+    "revision": "ae4c9fe91f6e6c81464b5214e233af5b",
+    "url": "/static/media/easycredit.ae4c9fe9.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "0e8d4c1095ee2353f85cf40717206e18",
+    "url": "/static/media/hnh.0e8d4c10.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d466986f3e29fca2eb43f3d1d6a97aab",
+    "url": "/static/media/manOnTable.d466986f.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "686e846bbb253a047a9d16526a10553b",
+    "url": "/static/media/ssi.686e846b.png"
+  },
+  {
+    "revision": "7db12bcf308a9734d4cc5bde836a9af5",
+    "url": "/static/media/succulentsbox.7db12bcf.png"
+  },
+  {
+    "revision": "dc04f6862a9133db906cd9a62913a9b8",
+    "url": "/static/media/succulentsbox.dc04f686.jpg"
+  }
+]);
